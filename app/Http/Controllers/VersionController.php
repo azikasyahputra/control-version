30a5998b1b7519ca18f6a78f7e77c663e74807b3
Azika Syahputra Azwar <azika.syahputra@gmail.com>
2025-09-22 03:15:10 +0700

    update all api test

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\DynamicKeyStoreRequest;
 use App\Services\VersionServices;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class VersionController extends Controller
 {    
@@ -26,16 +27,24 @@ class VersionController extends Controller
 
     public function store(DynamicKeyStoreRequest $request) : JsonResponse
     {
-        $dynamicData = $request->getDynamicKeyAndValue();
-        $storeVersionDto = StoreVersionData::fromArray($dynamicData);
-        $storeVersion = $this->versionServices->store($storeVersionDto);
-        return response()->json($storeVersion,201);
+        try{
+            $dynamicData = $request->getDynamicKeyAndValue();
+            $storeVersionDto = StoreVersionData::fromArray($dynamicData);
+            $storeVersion = $this->versionServices->store($storeVersionDto);
+            return response()->json($storeVersion,201);
+        }catch(ValidationException $exception){
+            return response()->json($exception->errors(), 422);
+        }
     }
 
     public function show(string $id, Request $request): JsonResponse
     {
-        $getVersionDto = GetVersionData::fromRequest($id,$request);
-        $versionData = $this->versionServices->find($getVersionDto);
-        return response()->json($versionData['data'],$versionData['code']);
+        try{
+            $getVersionDto = GetVersionData::fromRequest($id,$request);
+            $versionData = $this->versionServices->find($getVersionDto);
+            return response()->json($versionData['data'],$versionData['code']);
+        }catch(ValidationException $exception){
+            return response()->json($exception->errors(), 422);
+        }
     }
 }
